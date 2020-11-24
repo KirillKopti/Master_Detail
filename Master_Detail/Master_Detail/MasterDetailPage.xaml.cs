9227ed3937a875c3c36549c416aea44251fb20d6
Opilane TTHK <opilane@tthk.ee>
2020-11-24 12:32:04 +0200

    Add project files.

---
+++ Master_Detail/Master_Detail/MasterDetailPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Master_Detail
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MasterDetailPage : ContentPage
+    {
+        public MasterDetailPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
